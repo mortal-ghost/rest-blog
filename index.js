2ead29e67a4d0e6abfb50433d52e8f551ffadfd6
mortal-ghost <anmoldeepsingh148@gmail.com>
2022-03-19 16:43:59 +0530

    REST routes

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const sanitizer = require("express-sanitizer");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+mongoose.connect(
+  "mongodb+srv://ads_db:byebye21@cluster0.gzgym.mongodb.net/restblog?retryWrites=true&w=majority"
+);
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// Restful Routes
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { blogs: blogs });
+        }
+    });
+});
+
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+});
+
+app.post("/blogs", (req, res) => {
+    req.body.body = sanitize(req.body.body);
+
+    const newBlog = {
+        title: req.body.title,
+        image: req.body.image,
+        body: req.body.body
+    };
+
+    Blog.create(newBlog, (err, newBlog) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if (err) {
+            res.redirect('/blogs');
+        } else {
+            res.render('show', { foundBlog: foundBlog });
+        }
+    });
+});
+
+app.get('/blogs/:id/edit', (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if (err) {
+            res.redirect('/blogs');
+        } else {
+            res.render('edit', { foundBlog: foundBlog });
+        }
+    });
+});
+
+app.put('/blogs/:id', (req, res) => {
+    req.body.blog.body = sanitize(req.body.blog.body);
+
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+        if (err) {
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs/' + req.params.id);
+        }
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, (err) => {
+        if (err) {
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs');
+        }
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
